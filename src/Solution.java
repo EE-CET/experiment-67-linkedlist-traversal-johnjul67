7685a4b546675baa359a6933c5638ea4d45e95b5
John <tve24el033@cet.ac.in>
2026-04-09 18:30:47 +0530

    c1

--- src/Solution.java
+++ src/Solution.java
@@ -2,22 +2,34 @@ import java.util.LinkedList;
 import java.util.ListIterator;
 import java.util.Scanner;
 
-public class Solution {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+public class Solution 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
         
         // TODO: Read the integer N
-        
+        int N = sc.nextInt();
+        sc.nextLine();
         // TODO: Create a LinkedList of Integers
-        
+        LinkedList<Integer> list = new LinkedList<>();
         // TODO: Read N integers and add them to the LinkedList
-        
+        for(int i=0; i<N; i++) list.add(sc.nextInt());
         // TODO: Create a ListIterator for the LinkedList
-        
+        ListIterator<Integer> it = list.listIterator();
         // TODO: Traverse the list in the forward direction and print the elements
-        
+        System.out.print("Forward: ");
+        for(int j=0; j<N; j++) {
+          System.out.print(it.next());
+          if(j!=N-1) System.out.print(" ");
+        }
         // TODO: Traverse the list in the backward direction and print the elements
-        
-        
+        System.out.print("\nBackward: ");
+        for(int k = 0; k < N; k++) {
+          System.out.print(it.previous());
+          if(k!=N-1) System.out.print(" ");
+        }
+        sc.close();
     }
 }
+
